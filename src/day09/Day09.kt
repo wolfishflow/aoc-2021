667f1ef6f988264e36580ec0a09d704de606768b
Alan Simon <alancarlsimon@gmail.com>
2021-12-10 01:44:38 -0500

    Update Day09.kt
    refactored day 9

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -1,6 +1,7 @@
 package day09
 
 import readInput
+import java.lang.Exception
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -8,131 +9,110 @@ fun main() {
             line.toCharArray()
         }
 
-        val listOfLowPoints = mutableListOf<Int>()
-        matrix.forEachIndexed { y, row ->
-            row.forEachIndexed { x, digit ->
-                if (isLowPoint(x, y, matrix)) {
-                    listOfLowPoints.add(digit.digitToInt()+1)
+        val listOfPoints = mutableListOf<Point>().apply {
+            matrix.forEachIndexed { y, row ->
+                row.forEachIndexed { x, digit ->
+                    this += Point(x, y, digit.digitToInt())
                 }
             }
         }
 
-        return listOfLowPoints.sum()
+        val listOfLowPoints = listOfPoints.filter { point ->
+            getAdjPoints(point, matrix).all { newPoint ->
+                newPoint.heightVal > point.heightVal
+            }
+        }
+
+        return listOfLowPoints.sumOf { it.heightVal + 1 }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val matrix = input.map { line ->
+            line.toCharArray()
+        }
+
+        val listOfPoints = mutableListOf<Point>().apply {
+            matrix.forEachIndexed { y, row ->
+                row.forEachIndexed { x, digit ->
+                    this += Point(x, y, digit.digitToInt())
+                }
+            }
+        }
+
+        val listOfLowPoints = listOfPoints.filter { point ->
+            getAdjPoints(point, matrix).all { newPoint ->
+                newPoint.heightVal > point.heightVal
+            }
+        }
+
+        val threeLargestBasins = listOfLowPoints.map { findBasin(it, matrix) }.sortedBy { it.size }.takeLast(3)
+
+        return threeLargestBasins.map { it.size }.reduce { a, b -> a * b }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day09", "Day09_test")
     check(part1(testInput) == 15)
+    check(part2(testInput) == 1134)
 
     val input = readInput("day09", "Day09")
     println(part1(input))
     println(part2(input))
 }
 
-fun isLowPoint(x: Int, y: Int, matrix: List<CharArray>): Boolean {
-    val adjacentNumbers = getValidAdjNumbers(x, y, matrix)
-    val outcome = adjacentNumbers.filter { num -> num > matrix[y][x].digitToInt() }.size == adjacentNumbers.size
-    return outcome
+data class Point(val x: Int, val y: Int, val heightVal: Int)
+
+fun getAdjPoints(currentPoint: Point, matrix: List<CharArray>): List<Point> {
+    val up = getPossiblePoint(currentPoint.x, currentPoint.y - 1, matrix)
+    val left = getPossiblePoint(currentPoint.x - 1, currentPoint.y, matrix)
+    val right = getPossiblePoint(currentPoint.x + 1, currentPoint.y, matrix)
+    val down = getPossiblePoint(currentPoint.x, currentPoint.y + 1, matrix)
+
+    return listOfNotNull(up, left, right, down)
 }
 
-fun getValidAdjNumbers(x: Int, y: Int, matrix: List<CharArray>): List<Int> {
-    val numbers = mutableListOf<Int>()
 
-    if (x == 0) {
-        when (y) {
-            0 -> {
-                //top corner
-                //right
-                numbers.add(matrix[y][1].digitToInt())
-                //down
-                numbers.add(matrix[1][x].digitToInt())
-                return numbers
-            }
-            matrix.size - 1 -> {
-                // bottom corner
-                //up
-                numbers.add(matrix[y - 1][x].digitToInt())
-                //right
-                numbers.add(matrix[0][0].digitToInt())
-                return numbers
-            }
-            else -> {
-                //side wall
-                //up
-                numbers.add(matrix[y - 1][x].digitToInt())
-                //right
-                numbers.add(matrix[y][1].digitToInt())
-                //down
-                numbers.add(matrix[y + 1][x].digitToInt())
-                return numbers
-            }
-        }
-    } else if (x == matrix[y].size - 1) when (y) {
-        0 -> {
-            //top corner
-            //left
-            numbers.add(matrix[y][x - 1].digitToInt())
-            //down
-            numbers.add(matrix[1][x].digitToInt())
-            return numbers
-        }
-        matrix.size - 1 -> {
-            // bottom corner
-            //up
-            numbers.add(matrix[y - 1][x].digitToInt())
-            //left
-            numbers.add(matrix[y][x - 1].digitToInt())
-            return numbers
-        }
-        else -> {
-            //side wall
-            //up
-            numbers.add(matrix[y - 1][x].digitToInt())
-            //left
-            numbers.add(matrix[y][x - 1].digitToInt())
-            //down
-            numbers.add(matrix[y + 1][x].digitToInt())
-            return numbers
-        }
+fun getPossiblePoint(x: Int, y: Int, matrix: List<CharArray>): Point? {
+
+    if (x < 0 || x >= matrix.first().size || y < 0 || y >= matrix.size) {
+        return null
     }
 
-    when (y) {
-        0 -> {
-            //top wall
-            //left
-            numbers.add(matrix[y][x - 1].digitToInt())
-            //right
-            numbers.add(matrix[y][x + 1].digitToInt())
-            //down
-            numbers.add(matrix[1][x].digitToInt())
-            return numbers
-        }
-        matrix.size - 1 -> {
-            //bottom wall
-            //up
-            numbers.add(matrix[y - 1][x].digitToInt())
-            //left
-            numbers.add(matrix[y][x - 1].digitToInt())
-            //right
-            numbers.add(matrix[y][x + 1].digitToInt())
-            return numbers
-        }
-        else -> {
-            //interior piece - all 4 directions
-            //up
-            numbers.add(matrix[y - 1][x].digitToInt())
-            //left
-            numbers.add(matrix[y][x - 1].digitToInt())
-            //right
-            numbers.add(matrix[y][x + 1].digitToInt())
-            //down
-            numbers.add(matrix[y + 1][x].digitToInt())
-            return numbers
-        }
+    try {
+
+        return Point(x, y, matrix[y][x].digitToInt())
+
+    } catch (_: Exception) {
+
     }
+    return null
 }
+
+fun findBasin(point: Point, matrix: List<CharArray>): Set<Point> {
+    var current = mutableSetOf(point)
+
+    var next: MutableSet<Point>
+
+    while (true) {
+
+        next = getNextBasin(current, matrix)
+
+        if (current.size == next.size) {
+            return next
+        }
+
+        current = next
+    }
+
+}
+
+fun getNextBasin(current: MutableSet<Point>, matrix: List<CharArray>): MutableSet<Point> {
+    val nextBasin = current.toMutableSet()
+    for (point in current) {
+        nextBasin.addAll(getAdjPoints(point, matrix).filter { it.heightVal != 9 })
+    }
+    return nextBasin
+}
+
+
 
